61569ce649d69e5a7f417bcc58ceac01fd07db22
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2024-10-16 13:18:17 +0530

    First Commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,24 @@
+import { useEffect } from 'react'
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  useEffect( () => {
+    (
+      async () => {
+          const LocomotiveScroll = (await import('locomotive-scroll')).default
+          const locomotiveScroll = new LocomotiveScroll({
+            lenisOptions: {
+              duration: 2,
+            }
+          });
+      }
+    )()
+  }, []);
+
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
