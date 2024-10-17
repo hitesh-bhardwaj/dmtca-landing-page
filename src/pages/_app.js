c0db9d96eb1c4ddcd2ac1aab15534d6c556af31f
Hitesh Bhardwaj <bhardwajhitesh09@gmail.com>
2024-10-17 15:53:29 +0530

    UPDATE

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,30 @@
 import { useEffect } from 'react'
 import "@/styles/globals.css";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
 
-  useEffect( () => {
+  useEffect(() => {
     (
       async () => {
-          const LocomotiveScroll = (await import('locomotive-scroll')).default
-          const locomotiveScroll = new LocomotiveScroll({
-            lenisOptions: {
-              duration: 3.5
-            }
-          });
+        const LocomotiveScroll = (await import('locomotive-scroll')).default
+        const locomotiveScroll = new LocomotiveScroll({
+          lenisOptions: {
+            duration: 3.5
+          }
+        });
       }
     )()
   }, []);
 
   return (
     <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, max-scale=2" />
+        <title>DMTCA - Landing Page</title>
+        <meta name="description" content="DMTCA - Real Estate Landing Page"></meta>
+      </Head>
       <Component {...pageProps} />
     </>
   )
