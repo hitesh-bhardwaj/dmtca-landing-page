2d2eb6bb04b1771437ab9e12e3dcca68734b70b2
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2024-10-17 20:05:45 +0530

    update-(harsh)

--- src/components/Bringing.jsx
+++ src/components/Bringing.jsx
@@ -1,43 +1,98 @@
 import React, { useEffect, useRef } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-import { duration } from '@mui/material';
+import { useGSAP } from '@gsap/react';
 
-gsap.registerPlugin(ScrollTrigger);
+
+gsap.registerPlugin(ScrollTrigger,useGSAP);
 
 const Bringing = () => {
   const videoRef = useRef(null);
+ useGSAP(()=>{
+   const tl = gsap.timeline({
+    scrollTrigger:{
+      trigger:"#bringing",
+      pin:true,
+      start:"top top",
+      end:"+=2000 bottom",
+      scrub:0.5,
+      // markers:true
+    }
+   })
+   tl.to(".video",{
+    width:"80vw",
+    height:"40vw",
+    delay:0.1,
+    top:"15%",
+    ease:"none"
+   })
+   gsap.from(".text1",{
+    xPercent:120,
+    duration:3,
+    scrollTrigger:{
+      trigger:"#bringing",
+      scrub:0.5,
+      start:"top 60%",
+      end:"bottom bottom"
+    },
+    ease:"power2.inout"
+   })
+   gsap.from(".text2",{
+    xPercent:-240,
+    duration:4,
+    scrollTrigger:{
+      trigger:"#bringing",
+      scrub:0.5,
+      start:"top 60%",
+      end:"bottom bottom"
+    },
+    ease:"power2.inout"
+   })
+   gsap.from(".text3",{
+    xPercent:100,
+    duration:5,
+    scrollTrigger:{
+      trigger:"#bringing",
+      scrub:0.5,
+      start:"top 60%",
+      end:"bottom bottom"
+    },
+    ease:"power2.inout"
+   })
+   gsap.from(".video",{
+    xPercent:-400,
+    duration:5,
+    scrollTrigger:{
+      trigger:"#bringing",
+      scrub:true,
+      start:"top 60%",
+      end:"bottom bottom"
+    },
+    
+    ease:"power2.inout"
+   })
 
-  const tl = gsap.timeline();
 
+ })
 
   return (
-    <section id='bringing'>
-      <div className='container-lg w-screen h-full py-[5%]'>
-        <div className='flex flex-col items-center justify-center gap-[5vw]'>
-          <div className='flex flex-col items-center justify-center text1'>
+    <section id='bringing' className='w-screen h-[70vw]'>
+      <div className='container-lg w-full h-full  py-[5%] relative'>
+        <div className='flex flex-col items-center justify-center w-full h-full gap-[5vw]'>
+          <div className='flex flex-col items-center justify-center '>
+            <div className='text1'>
             <p className='text-[10.5vw] text-[#353430] leading-[1.1] text-head uppercase'>BRINGING YOU</p>
-            <div className='flex justify-center gap-[3vw] text2'>
-              <p className='text-[10.5vw] text-[#353430] leading-[1.1] text-head uppercase'>SPACES</p>
-              <div
-                className='h-[13vw] w-[20vw] rounded-[20px] cover overflow-hidden relative video'
-                ref={videoRef}
-              >
-                <video
-                  src='/videos/bringing.mp4'
-                  autoPlay
-                  muted
-                  loop
-                  className='w-full h-full'
-                />
               </div>
+            <div className='flex justify-center gap-[3vw] text2'>
+              <p className='text-[10.5vw] text-[#353430] leading-[1.1] text-head uppercase '>SPACES</p>
+              
             </div>
             <div className='text3'>
               <p className='text-[10.5vw] text-[#353430] leading-[1.1] text-head uppercase'>YOU LOVE</p>
             </div>
           </div>
 
-          <div className='w-[75%]'>
+          <div className='w-full mt-[5vw]'>
             <p className='text-body text-[1.3vw] text-center'>
               At DMTCA, we artfully combine design brilliance, innovative technology, and human insight
               to deliver unparalleled real estate experiences. Our approach goes beyond conventional sales,
@@ -48,6 +103,18 @@ const Bringing = () => {
             </p>
           </div>
         </div>
+        <div
+                className='h-[13vw] w-[20vw] rounded-[20px] cover overflow-hidden absolute top-[35%] translate-y-[-35%] right-[10%] video'
+                ref={videoRef}
+              >
+                <video
+                  src='/videos/bringing.mp4'
+                  autoPlay
+                  muted
+                  loop
+                  className='w-full h-full object-cover'
+                />
+              </div>
       </div>
     </section>
   );
