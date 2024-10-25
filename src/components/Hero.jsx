88ba285387b8c0ca42dbb3dad454cb20e70f7f9e
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-10-25 15:08:43 +0530

    update
    dynamic load - enquiry form

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
-import React from "react";
+import React, { Suspense } from "react";
+import dynamic from "next/dynamic";
 import PrimaryButton from "./Button/PrimaryButton";
 import Link from "next/link";
-import EnquiryForm from "./Enquiryform";
+
+const EnquiryForm = dynamic(() => import("./Enquiryform"), { suspense: true });
 
 const Hero = () => {
   return (
@@ -17,7 +19,8 @@ const Hero = () => {
                 alt="logo-img"
                 width={160}
                 height={24}
-                className="object-contain mobile:w-[30vw] tablet:w-[20vw]" />
+                className="object-contain mobile:w-[30vw] tablet:w-[20vw]"
+              />
             </Link>
             <Link href="/property1" className="block fadeup">
               <Image
@@ -26,7 +29,8 @@ const Hero = () => {
                 alt="logo-img"
                 width={140}
                 height={48}
-                className="object-contain mobile:w-[28vw] tablet:w-[20vw]" />
+                className="object-contain mobile:w-[28vw] tablet:w-[20vw]"
+              />
             </Link>
           </div>
 
@@ -41,7 +45,13 @@ const Hero = () => {
             className="object-cover w-full h-full object-top relative z-[-1]"
           />
           <div className="w-[55%] flex flex-col gap-[6vw] mt-[15vw] mb-[8vw] tablet:w-[90%] tablet:mt-[20vh] tablet:mb-[12vw] tablet:gap-[12vw]">
-            <p data-para-anim className="text-[1.5vw] text-white tracking-wide opacity-90 w-[80%] mobile:text-[4.5vw] tablet:text-[3.2vw]">Experience Iconic Luxury Living at Dubai&apos;s Most Prestigious and Exclusive Address</p>
+            <p
+              data-para-anim
+              className="text-[1.5vw] text-white tracking-wide opacity-90 w-[80%] mobile:text-[4.5vw] tablet:text-[3.2vw]"
+            >
+              Experience Iconic Luxury Living at Dubai&apos;s Most Prestigious
+              and Exclusive Address
+            </p>
             <div className="w-full h-[1px] bg-white lineDraw"></div>
           </div>
           <div className="relative z-10">
@@ -49,10 +59,36 @@ const Hero = () => {
               <span data-para-anim>Live In</span>
               <div className="flex items-start gap-[1.5vw] tablet:flex-col tablet:items-start tablet:gap-[4vw]">
                 <span data-para-anim>Grandeur</span>
-                <a aria-label="Select Title" data-scroll data-scroll-to data-scroll-duration="2s" href="#about" className="w-fit mt-[0.5vw] tablet:mt-0 h-fit p-[2vw] rounded-full flex justify-center items-center border-[2px] border-white cursor-pointer fadeup tablet:p-[4vw]">
-                  <svg viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg" className="btn-icon rotate-90 w-[3vw] h-[3vw] tablet:w-[7vw] tablet:h-[7vw]">
-                    <path data-v-f4363f2a fillRule="evenodd" clipRule="evenodd" d="M3.82475e-07 5.625L7.625 5.625L4.125 9.125L5 10L10 5L5 -4.37114e-07L4.125 0.874999L7.625 4.375L4.91753e-07 4.375L3.82475e-07 5.625Z" className="btn-path" fill="white" />
-                    <path data-v-f4363f2a fillRule="evenodd" clipRule="evenodd" d="M3.82475e-07 5.625L7.625 5.625L4.125 9.125L5 10L10 5L5 -4.37114e-07L4.125 0.874999L7.625 4.375L4.91753e-07 4.375L3.82475e-07 5.625Z" className="btn-path" fill="white" />
+                <a
+                  aria-label="Select Title"
+                  data-scroll
+                  data-scroll-to
+                  data-scroll-duration="2s"
+                  href="#about"
+                  className="w-fit mt-[0.5vw] tablet:mt-0 h-fit p-[2vw] rounded-full flex justify-center items-center border-[2px] border-white cursor-pointer fadeup tablet:p-[4vw]"
+                >
+                  <svg
+                    viewBox="0 0 10 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="btn-icon rotate-90 w-[3vw] h-[3vw] tablet:w-[7vw] tablet:h-[7vw]"
+                  >
+                    <path
+                      data-v-f4363f2a
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M3.82475e-07 5.625L7.625 5.625L4.125 9.125L5 10L10 5L5 -4.37114e-07L4.125 0.874999L7.625 4.375L4.91753e-07 4.375L3.82475e-07 5.625Z"
+                      className="btn-path"
+                      fill="white"
+                    />
+                    <path
+                      data-v-f4363f2a
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M3.82475e-07 5.625L7.625 5.625L4.125 9.125L5 10L10 5L5 -4.37114e-07L4.125 0.874999L7.625 4.375L4.91753e-07 4.375L3.82475e-07 5.625Z"
+                      className="btn-path"
+                      fill="white"
+                    />
                   </svg>
                 </a>
               </div>
@@ -60,12 +96,24 @@ const Hero = () => {
           </div>
           <div className="flex flex-col my-[10vw] gap-[6vw] mobile:gap-[10vw] mobile:mt-[20vw] tablet:gap-[5vw] tablet:mt-[10vw]">
             <div className="fadeup">
-              <PrimaryButton link={"#"} btnText={"Enquire Today"} className="shadow-2xl drop-shadow-2xl " />
+              <PrimaryButton
+                link={"#"}
+                btnText={"Enquire Today"}
+                className="shadow-2xl drop-shadow-2xl "
+              />
             </div>
-            <p data-para-anim className="text-[1.5vw] text-white opacity-90 tracking-wide w-[40%] mobile:text-[4.5vw] tablet:w-[70%] tablet:text-[3.2vw]">We Elevate Your Lifestyle With World-Class Architectural Creations</p>
+            <p
+              data-para-anim
+              className="text-[1.5vw] text-white opacity-90 tracking-wide w-[40%] mobile:text-[4.5vw] tablet:w-[70%] tablet:text-[3.2vw]"
+            >
+              We Elevate Your Lifestyle With World-Class Architectural
+              Creations
+            </p>
           </div>
           <div className="mobile:hidden">
-            <EnquiryForm/>
+            <Suspense fallback={null}>
+              <EnquiryForm />
+            </Suspense>
           </div>
         </div>
       </section>
