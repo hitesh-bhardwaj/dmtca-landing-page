6d84b829af9ad6e62bd4c3845235fffd10eca567
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2024-10-29 17:10:21 +0530

    Update index.js

--- src/pages/property1/index.js
+++ src/pages/property1/index.js
@@ -5,7 +5,7 @@ import Service from "@/components/Service";
 import Project from "@/components/Project";
 import Footer from "@/components/Footer";
 import ShowCase from "@/components/ShowCase";
-import Bringing from "@/components/Bringing";
+// import Bringing from "@/components/Bringing";
 import { paraAnim, lineAnim, fadeUp } from "@/components/gsapAnimations";
 import Gallery from "@/components/Gallery";
 import ProjectHover from "@/components/ProjectHover";
