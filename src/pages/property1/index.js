2f73c381785fe90b74d522135a7039c52e8ca1f9
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2024-10-29 16:54:42 +0530

    test

--- src/pages/property1/index.js
+++ src/pages/property1/index.js
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
 import Works from "@/components/Works";
-import SlideShow from "@/components/SlideShow";
+// import SlideShow from "@/components/SlideShow";
 import Service from "@/components/Service";
 import Project from "@/components/Project";
 import Footer from "@/components/Footer";
@@ -10,13 +10,14 @@ import { paraAnim, lineAnim, fadeUp } from "@/components/gsapAnimations";
 import Gallery from "@/components/Gallery";
 import ProjectHover from "@/components/ProjectHover";
 import dynamic from "next/dynamic";
-import Loader from "@/components/Loader";
+// import Loader from "@/components/Loader";
 
 
 const DynamicAbout = dynamic(() => import('@/components/About'), {
   // loading: () => <p>Loading...</p>,
   
 })
+const SlideShow = dynamic(()=>import('@/components/SlideShow'))
 
 export default function Home() {
   paraAnim();
