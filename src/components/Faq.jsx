cc2655b0a3078c2f17b27b232c77df16466a3605
Harshgoyal-WebDev <138307903+Harshgoyal-WebDev@users.noreply.github.com>
2024-10-30 13:20:25 +0530

    update-(harsh)

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -15,14 +15,14 @@ const SingleAccordion = ({ id, title, content1, content2 }) => {
     return (
       <AccordionItem value={id} className="group relative">
         <div className="w-full bg-black lineDraw h-[1px]  mobile:block tablet:block"></div>
-        <div className="absolute bg-black w-full h-full scale-y-0 group-hover:scale-y-[1] origin-top transition-all duration-500 ease-out"></div>
+        <div className="absolute bg-[#E0D4C6] w-full h-full scale-y-0 group-hover:scale-y-[1] origin-top transition-all duration-500 ease-out"></div>
         <AccordionTrigger
           
-          className="text-[1.5vw] font-semibold mobile:w-[70%] mobile:text-[5vw] pl-[2vw] mobile:text-left mobile:flex tablet:text-[3.5vw] relative z-[2] tablet:text-left group-hover:text-white ease-in py-[2%] mobile:py-[5%] accordion [&[data-state=open]>.line>.line-internal>.icon-container>.icon]:rotate-[90deg] [&[data-state=open]>.line>.line-internal>.icon-container>.icon>.minus]:rotate-90 [&[data-state=open]>.line>.line-internal>.icon-container>.icon]:bg-body [&[data-state=open]>.line>.line-internal>.icon-container>.icon]:text-white"
+          className="text-[1.5vw] font-semibold mobile:w-[70%] mobile:text-[5vw] pl-[2vw] mobile:text-left mobile:flex tablet:text-[3.5vw] relative z-[2] tablet:text-left ease-in py-[2%] mobile:py-[5%] accordion [&[data-state=open]>.line>.line-internal>.icon-container>.icon]:rotate-[90deg] [&[data-state=open]>.line>.line-internal>.icon-container>.icon>.minus]:rotate-90 [&[data-state=open]>.line>.line-internal>.icon-container>.icon]:bg-body [&[data-state=open]>.line>.line-internal>.icon-container>.icon]:text-white"
         >
           {title}
         </AccordionTrigger>
-        <AccordionContent className="text-[1.2vw] pl-[2vw] text-black font-light leading-[1.3] space-y-[1.5vw] w-[70%] mb-[20px] mobile:w-full mobile:text-[4vw] mobile:space-y-[4vw] group-hover:text-white relative z-[2] tablet:text-[2.7vw] tablet:w-[90%]">
+        <AccordionContent className="text-[1.2vw] pl-[2vw] text-black font-light leading-[1.3] space-y-[1.5vw] w-[70%] mb-[20px] mobile:w-full mobile:text-[4vw] mobile:space-y-[4vw] relative z-[2] tablet:text-[2.7vw] tablet:w-[90%]">
           <p data-para-anim className="">
             {content1}
           </p>
